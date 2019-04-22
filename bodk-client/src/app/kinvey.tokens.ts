2d68e4de3c3f6e061224643f88eb2a447eea19ed
mariakancheva <mariakancheva@users.noreply.github.com>
2019-04-22 08:42:24 +0300

    Delete kinvey.tokens.ts

--- bodk-client/src/app/kinvey.tokens.ts
+++
@@ -1,4 +0,0 @@
-export const APP_KEY = 'kid_Hyp7-fX9N' // Kinvey APP KEY here;
-export const APP_SECRET = '5d1e8cca3ad94deca39d4966dc9d78b4' // Kinvey APP SECRET here;
-
-export const ADMIN_ROLE_ID = '6761d85e-f664-466c-b326-4189c3ec65ac'
